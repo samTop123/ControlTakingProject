331e0d77ade2198d9c1a8f0da7962368f3d28e28
samTop123 <barsam618@gmail.com>
2025-04-15 22:14:03 +0300

    keyboard and mouse sending with threading data

--- keyboard_getting_client.py
+++ keyboard_getting_client.py
@@ -1,21 +1,50 @@
 import socket
 import keyboard
 import win32api
+import win32con
 
-if __name__ == "__main__":
+def mouse_click(button, action):
+    if button == 'left':
+        if action == 'down':
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+        else:
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+    elif button == 'right':
+        if action == 'down':
+            win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0)
+        else:
+            win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0)
+
+def move_mouse(x, y):
+    win32api.SetCursorPos((x, y))
+
+if __name__ == '__main__':
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('0.0.0.0', 9999))
+    client.connect(('SERVER_IP_HERE', 9999))  # Replace with server IP
+    client.send("Hello from client!".encode())
 
-    client.send('hello From Client !'.encode())
-
+    buffer = ""
     while True:
         try:
-            data = client.recv(1).decode()
-            print(data)
-            if str(data) == "1":
-                data = client.recv(1024).decode().strip()
-                if data:
-                    print(f"Executing: {data}")
-                    keyboard.press_and_release(data)
-        except:
+            data = client.recv(1024).decode()
+            if not data:
+                break
+
+            buffer += data
+            while '\n' in buffer:
+                line, buffer = buffer.split('\n', 1)
+                parts = line.strip().split('|')
+
+                if not parts:
+                    continue
+
+                if parts[0] == 'MOUSE_MOVE':
+                    x, y = int(parts[1]), int(parts[2])
+                    move_mouse(x, y)
+                elif parts[0] == 'MOUSE_CLICK':
+                    mouse_click(parts[1], parts[2])
+                elif parts[0] == 'KEY':
+                    keyboard.press_and_release(parts[1])
+        except Exception as e:
+            print("[Error]", e)
             break
--- keyboard_sending_server.py
+++ keyboard_sending_server.py
@@ -1,10 +1,12 @@
 import socket
 import keyboard
-import mouse
+import win32api
+import win32con
+import time
+import threading
 
-def on_key(event):
+def send_key_event(client, event):
     if event.event_type == 'down':
-        client.send("1".encode())
         modifiers = []
         if keyboard.is_pressed('ctrl'):
             modifiers.append('ctrl')
@@ -13,21 +15,50 @@ def on_key(event):
         if keyboard.is_pressed('shift'):
             modifiers.append('shift')
 
-        # Don't include modifier key by itself (like 'ctrl' with no other key)
-        if event.name not in modifiers:
-            modifiers.append(event.name)
-            combo = '+'.join(modifiers)
+        key = event.name
+        if key not in modifiers:
+            combo = '+'.join(modifiers + [key]) if modifiers else key
             try:
-                client.send((combo + '\n').encode())
+                client.send(f"KEY|{combo}\n".encode())
             except:
-                pass
+                print("Key send failed.")
+
+def track_mouse(client):
+    prev_pos = (0, 0)
+    prev_left = prev_right = False
+
+    while True:
+        x, y = win32api.GetCursorPos()
+        left = win32api.GetKeyState(win32con.VK_LBUTTON) < 0
+        right = win32api.GetKeyState(win32con.VK_RBUTTON) < 0
+
+        # Movement
+        if (x, y) != prev_pos:
+            client.send(f"MOUSE_MOVE|{x}|{y}\n".encode())
+            prev_pos = (x, y)
+
+        # Clicks
+        if left != prev_left:
+            action = 'down' if left else 'up'
+            client.send(f"MOUSE_CLICK|left|{action}\n".encode())
+            prev_left = left
+
+        if right != prev_right:
+            action = 'down' if right else 'up'
+            client.send(f"MOUSE_CLICK|right|{action}\n".encode())
+            prev_right = right
+
+        time.sleep(0.01)
 
 if __name__ == '__main__':
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(('0.0.0.0', 9999))
     server.listen(1)
+    print("[*] Waiting for client...")
     client, addr = server.accept()
+    print("[+] Connected:", addr)
+    print(client.recv(1024).decode())  # Greeting
 
-    print(client.recv(1024).decode())  # Receive greeting
-    keyboard.hook(on_key)
-    keyboard.wait()  # Keep running
+    threading.Thread(target=track_mouse, args=(client,), daemon=True).start()
+    keyboard.hook(lambda e: send_key_event(client, e))
+    keyboard.wait()
