76eff38acf3865f143742a49fa7f6133a65a3d02
samTop123 <barsam618@gmail.com>
2025-04-15 21:26:44 +0300

    keyboard sending data and combos

--- keyboard_getting_client.py
+++ keyboard_getting_client.py
@@ -1,22 +1,21 @@
 import socket
 import keyboard
+import win32api
 
 if __name__ == "__main__":
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    hostname = socket.gethostname()
-    IPAddr = socket.gethostbyname(hostname)
-
-    client.connect((IPAddr, 9999))
+    client.connect(('0.0.0.0', 9999))
 
     client.send('hello From Client !'.encode())
-    # Not receiving any response in this version
 
     while True:
-        data = client.recv(1024).decode().strip()
-        if data:
-            try:
-                keyboard.press_and_release(data)
-                print(f"Pressed: {data}")
-            except:
-                print(f"Invalid key: {data}")
+        try:
+            data = client.recv(1).decode()
+            print(data)
+            if str(data) == "1":
+                data = client.recv(1024).decode().strip()
+                if data:
+                    print(f"Executing: {data}")
+                    keyboard.press_and_release(data)
+        except:
+            break
--- keyboard_sending_server.py
+++ keyboard_sending_server.py
@@ -1,27 +1,33 @@
-import threading
-import keyboard
 import socket
+import keyboard
+import mouse
 
-def send_keys(client):
-    def on_key(event):
-        try:
-            client.send(f"{event.name}\n".encode())
-        except:
-            pass  # connection might be closed
+def on_key(event):
+    if event.event_type == 'down':
+        client.send("1".encode())
+        modifiers = []
+        if keyboard.is_pressed('ctrl'):
+            modifiers.append('ctrl')
+        if keyboard.is_pressed('alt'):
+            modifiers.append('alt')
+        if keyboard.is_pressed('shift'):
+            modifiers.append('shift')
 
-    keyboard.on_press(on_key)
-    keyboard.wait()  # Keep the thread alive
+        # Don't include modifier key by itself (like 'ctrl' with no other key)
+        if event.name not in modifiers:
+            modifiers.append(event.name)
+            combo = '+'.join(modifiers)
+            try:
+                client.send((combo + '\n').encode())
+            except:
+                pass
 
 if __name__ == '__main__':
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(('0.0.0.0', 9999))
-    server.listen(5)
+    server.listen(1)
     client, addr = server.accept()
 
-    print(client.recv(1024).decode())  # greeting
-
-    # Start sending key events in another thread
-    threading.Thread(target=send_keys, args=(client,), daemon=True).start()
-
-    while True:
-        pass  # Keep main thread alive
+    print(client.recv(1024).decode())  # Receive greeting
+    keyboard.hook(on_key)
+    keyboard.wait()  # Keep running
