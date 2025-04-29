5375d4aacc762f13f26afe1d7a494289cd2acb8a
samTop123 <barsam618@gmail.com>
2025-04-29 23:10:53 +0300

    improved structure

--- server_screenshots.py
+++ server_screenshots.py
@@ -8,6 +8,19 @@ from win32api import GetSystemMetrics
 SERVER_IP = constants.SERVER_IP
 UDP_PORT = constants.UDP_PORT
 
+def decode_image_from_mem(received_data) -> ImageTk.PhotoImage:
+    # Decode image from memory
+    image_stream = BytesIO(received_data)
+    img = Image.open(image_stream)
+    img = img.resize((GetSystemMetrics(0), int(GetSystemMetrics(1)*0.9)))
+    tk_img = ImageTk.PhotoImage(img)
+
+    return tk_img
+
+def display_on_canvas(my_canvas, tk_img):
+    my_canvas.image = tk_img
+    my_canvas.create_image(0, 0, image=tk_img, anchor='nw')
+
 def update(count, root, my_canvas, server):
     try:
         # Receive image size safely
@@ -25,15 +38,9 @@ def update(count, root, my_canvas, server):
             data, _ = server.recvfrom(1024)
             received_data += data
 
-        # Decode image from memory
-        image_stream = BytesIO(received_data)
-        img = Image.open(image_stream)
-        img = img.resize((GetSystemMetrics(0), int(GetSystemMetrics(1)*0.9)))
-        tk_img = ImageTk.PhotoImage(img)
+        tk_img = decode_image_from_mem(received_data)
 
-        # Display on canvas
-        my_canvas.image = tk_img
-        my_canvas.create_image(0, 0, image=tk_img, anchor='nw')
+        display_on_canvas(my_canvas, tk_img)
 
     except Exception as e:
         print(f"Error loading image: {e}")
